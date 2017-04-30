ed437f43e09153e9a69acd7b6bdadb434e1e2de0
Kai Cataldo <kaicataldo@gmail.com>
2017-04-29 22:46:43 -0400

    Update hot module reloading config for Webpack 2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+/* global module */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 import App from './components/App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+function render(Component) {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+}
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App);
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,46 @@
 /* eslint-env node */
 
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  devtool: process.env.NODE_ENV === 'production' ? 'cheap-module-source-map' : 'eval',
+  devtool: process.env.NODE_ENV === 'production' ? 'cheap-module-source-map' : 'inline-source-map',
   entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
     'babel-polyfill',
     './src/index.js'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
     filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/dist/'
   },
   module: {
     rules: [{
       test: /\.js$/,
-      loader: 'eslint-loader',
-      exclude: /node_modules/,
-      enforce: 'pre'
-    },
-    {
-      test: /\.js$/,
       use: [
-        'react-hot-loader',
         'babel-loader'
       ],
-      include: path.join(__dirname, 'src'),
+      include: path.resolve(__dirname, 'src'),
       exclude: /node_modules/
     }]
   },
   resolve: {
     extensions: ['.js'],
     modules: [
-      path.join(__dirname, 'src'),
+      path.resolve(__dirname, 'src'),
       'node_modules'
     ]
   },
   devServer: {
-    quiet: false,
-    noInfo: false,
-    stats: {
-      assets: false,
-      colors: true,
-      version: false,
-      hash: false,
-      timings: false,
-      chunks: false,
-      chunkModules: false
-    }
-  }
+    hot: true,
+    contentBase: path.resolve(__dirname),
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+  ]
 };
